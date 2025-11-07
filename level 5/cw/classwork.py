87035a9c7d86140408468d1a3abd27bdbd0db8ba
dr362 <sandrogabidzashvili98@gmail.com>
2025-11-07 20:39:58 +0400

    commit

---
+++ level 5/cw/classwork.py
@@ -0,0 +1,44 @@
+#1)კარგი პრაქტიკა - უკეთესი ვერსია კოდის
+
+#2)
+age = 10
+age = 11
+print(age)
+
+#3)
+Alexandre = hi
+ALEXANDRE = HI
+alexandre = Hi
+print(Alexandre)
+print(ALEXANDRE)
+print(alexandre)
+
+#4)
+num1 = 10
+num2 = 5
+num3 = num1 * num2
+print(num3)
+
+#5)
+print(7634 % 4)
+print(982 % 3)
+print(12345 % 3)
+
+#6)
+name = "giorgi"
+print(name)
+name = "vano"
+Name = "nika"
+NAME = "saba"
+print(Name)
+
+#კოდი გამოიტანს nika-ს
+
+#7)
+age = 23
+print(age)
+
+num = "34"
+print(num + "5")    
+
+print(5 *  3)
