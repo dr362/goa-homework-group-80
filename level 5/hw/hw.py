924c94b00effbcd4110e844fca0c6249f01a8966
dr362 <sandrogabidzashvili98@gmail.com>
2025-11-23 19:31:09 +0400

    fifth commit

---
+++ level 5/hw/hw.py
@@ -0,0 +1,44 @@
+#1) კომენტარის სახით ახსენი რას ნიშნავს debugging
+#debbuging - ანუ რაიმე ერორის ან ბაგების გასწორება
+
+#2) შექმენით ცვლადი სახელად name რომელსაც მიანიჭებ შენს ასაკს, განაახლე მისი მნიშვნელობა და დაპრინტე
+name = 11
+name = sandro
+print(name)
+
+#3) შექმენით ორი ცვლადი სადაც შეინახავ სტრინგებს, შექმენით მესამე ცვლადი სადაც ამ ორ ცვლადს კონკატინაციას გაუკეთებთ და დაპრინტე 
+string = "string"
+String = "String"
+string_String = string + String
+print(string_String)
+
+#4) გამოთვალე რა იქნება ნაშთი: 4564-ის 4-ზე გაყოფისას, 9768-ის 3-ზე გაყოფისას, 16785-ის 3-ზე გაყოფისას
+print(4564 % 4)
+print(9768 % 3)
+print(16785 % 3)
+
+# #5) გაასწორე შემდეგი კოდები:
+# name = "saba"
+# name = "giorgi"
+# print(Name)
+
+
+#num = "40"
+
+#print(num * "3")
+
+#სწორი ვერსია
+name = "saba"
+name = "giorgi"
+print(name)
+
+num = 40
+
+print(num * 3)
+# ან
+name = "saba"
+Name = "giorgi"
+print(Name)
+
+num = "40"
+print(num * 3)
---
+++ level 7/hw/hw.py
@@ -0,0 +1,27 @@
+# 1)მომხმარებელს შემოატანინეთ მისი ასაკი და გაიგეთ რამდენჯერ მოთავსდება რიცხვი 10 მასში
+
+age = input("Please enter your age: ")
+print(int(age) % 10)
+
+# 2)მომხმარებელს შემოატანინეთ ორი რიცხვი (num1 და num2) და გაიგეთ რა იქნება მათი ნამრავლი
+
+num1 = input("your number is...: ")
+num2 = input("your 2nd number is...: ")
+print(int(num1) * int(num2))
+
+# 3)მომხმარებელს შემოატანინეთ მისი ასაკი და შემდეგ გადააქციეთ ინტეჯერად
+
+integ = input("please enter your 2nd age? : ")
+print(int(integ))
+
+# 4)მოხმარებელს შემოატანინეთ 3 რიცხვი ანუ num , num1 , num2...და ასე შემდეგ და საბოლოოდ გამოითვალეთ მაგ რიცხების საშუალო არითმეტიკული
+
+num = int(input("num: "))
+Num1 = int(input("num: "))
+Num2 = int(input("num: "))
+print(num + Num1 + Num2)
+
+# 5)დაწერეთ პროგრამა რომელიც მომხმარებელს სთხოვს მის დაბადების წელს და გამოითვლის მის წლოვანებას
+
+year = int(input("your were gaven birth in : "))
+print(2025 - year)
