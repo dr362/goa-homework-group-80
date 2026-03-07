615a9fc2858854ab855145027bd31ef743090447
dr362 <sandrogabidzashvili98@gmail.com>
2026-03-07 10:26:27 +0400

    22'nd commit

---
+++ level 21/hw/hw.py
@@ -0,0 +1,25 @@
+# 1) ახსენით რა არის არგუმენტი და ფუნქცია
+#argument - ისეთი რამ რასაც ვიყენებთ იმისთვის რომ ფუნქცია გამოვიყენოთ
+#function - ფუნქცია გვეხმარება ტერმინალში რაიმის გამოსატანად
+# 2)დაწერეთ ყველა ფუნქციაზე 3-3 მაგალითი
+print("hi")
+print(True)
+print(1)
+#-----------
+name = input("Please enter your name: ")
+print(name)
+age = input("Please enter your age: ")
+print(int(age))
+true_or_false = input("True Or False: ")
+print(true_or_false)
+#-----------
+print(type("INTEGER"))
+print(type(254986))
+print(type(True))
+#-----------
+for i in range(0,10,2):
+    print(i)
+for i in range(0,30,2):
+    print(i)
+for i in range(5,10,2):
+    print(i)
